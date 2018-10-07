1ba9f52de36aa4b83ac7125208828eb6e0c9baa8
rodolphe poon <poon.rodolphe@hotmail.fr>
2018-10-07 12:05:48 +0200

    level2

--- fullstack_1/frontend/src/App.js
+++ fullstack_1/frontend/src/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-import { Chart } from "react-google-charts";
+import Chart from 'react-google-charts';
 
 const OBJECTIVES = [
       { "id": 1, "title": "First objective", "start": 0, "target": 50, "current": 20, "start_date": "2018-01-05", "end_date": "2018-03-05" },
@@ -16,49 +15,37 @@ const OBJECTIVES = [
 
     const result = OBJECTIVES.filter(objective => objective.current > objective.target)
 
-    
+    //const data =OBJECTIVES.map(e=>[{v:e.id.toString(),f:e.title},e.parent_id?e.parent_id.toString():""])
 
+    const data = [[
+        { type: 'string', label: 'Task ID' },
+        { type: 'string', label: 'Task Name' },
+        { type: 'date', label: 'Start Date' },
+        { type: 'date', label: 'End Date' },
+        { type: 'number', label: 'Duration' },
+        { type: 'number', label: 'Percent Complete' },
+        { type: 'string', label: 'Dependencies' }
+        ],
+        ...OBJECTIVES.map(e=>[e.id.toString(),e.title,new Date(e.start_date),new Date(e.end_date),new Date(e.end_date)-new Date(e.start_date),100*(e.current-e.start)/Math.abs(e.target-e.start),e.parent_id?e.parent_id.toString():""])
+      ]
+    
 
 class App extends Component {
 
-  
-  expectation(date,x1,y1,x2,y2){
 
-    return (y2-y1)/(x2-x1)*(date-x1)+y1
-  }
 
-     lineChart=(obj)=>{
-
-      let expectedValue= this.expectation(new Date(TODAY),new Date(obj.start_date),obj.start,new Date(obj.end_date),obj.target)
-      const dataTest =[[
-      { type: 'date', label: 'Time' },
-      'target','current'
-    ],
-    [new Date(obj.start_date),obj.start,obj.start],
-    [new Date(TODAY),expectedValue,obj.current],
-    [new Date(obj.end_date),obj.target,null],
-    ]
-
-    return(
-    <div style={{height:200,width:400, border:'1px solid black'}}>
-     <Chart
-          chartType="LineChart"
-          data={dataTest}
-          options={{
-    hAxis: {
-      title: obj.title,
-    }}}
-          width="100%"
-          height="100%"
-        />
-    </div>
-    )
-  }
   render() {
     return (
      <div>
       <div>{result.length} objectives have their current value over their target</div>
-      <div>{OBJECTIVES.map(objective=>(<div key={objective.title}>{this.lineChart(objective)}</div>))} </div>
+    
+    <Chart
+  width={'100%'}
+  height={'500px'}
+  chartType="Gantt"//OrgChart/Gantt
+  loader={<div>Loading Chart</div>}
+  data={data}
+/>
     </div>
     );
   }
